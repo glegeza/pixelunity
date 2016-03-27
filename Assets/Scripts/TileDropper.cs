8beb0ef1259cf158799664244469c7c4e58a4055
George Legeza <glegeza@gmail.com>
2016-03-27 19:21:01 -0400

    Add mouse control for dropping and destroying tiles

--- Assets/Scripts/TileDropper.cs
+++ Assets/Scripts/TileDropper.cs
@@ -3,25 +3,30 @@ using System.Collections.Generic;
 
 public class TileDropper : MonoBehaviour
 {
-    public KeyCode DropKey;
     public KeyCode ResetKey;
     public List<GameObject> TilePrefabs;
     public int SelectedTile = 0;
     public bool RandomTile = true;
 
+    private MouseObjectTracker _tracker;
     private PixelMouse _mouse;
     private List<GameObject> _tiles = new List<GameObject>();
     
 	void Start ()
     {
+        _tracker = FindObjectOfType<MouseObjectTracker>();
         _mouse = FindObjectOfType<PixelMouse>();
 	}
 	
 	void Update ()
     {
-        if (Input.GetKeyDown(DropKey))
+        if (Input.GetMouseButtonDown(0) && _tracker.CurrentObject == null)
         {
             DropAtLocation();
+        }
+        else if (Input.GetMouseButtonDown(1) && _tracker.CurrentObject != null && _tracker.CurrentObject.GetComponent<HoverColorizer>() != null)
+        {
+            Destroy(_tracker.CurrentObject);
         }
         else if (Input.GetKeyDown(ResetKey))
         {
