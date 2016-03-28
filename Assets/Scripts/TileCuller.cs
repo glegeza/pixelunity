580df172ccdf892affa48ad91902ecc5ed0a4e0a
George Legeza <glegeza@gmail.com>
2016-03-27 20:54:13 -0400

    Add script to cull tiles that fall out of the world

---
+++ Assets/Scripts/TileCuller.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class TileCuller : MonoBehaviour
+{
+    [Tooltip("Lifetime of the tile once it is no longer visible to the camera.")]
+    public float CullTime = 2.0f;
+
+    private SpriteRenderer _renderer;
+    private float _timeUnseen;
+    
+	void Start ()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+	}
+	
+	void Update ()
+    {
+        if (_renderer.isVisible)
+        {
+            _timeUnseen = 0.0f;
+            return;
+        }
+        else
+        {
+            _timeUnseen += Time.deltaTime;
+        }
+
+        if (_timeUnseen > CullTime)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
