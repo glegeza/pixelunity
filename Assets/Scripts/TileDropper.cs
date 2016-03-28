150d848b3301057a362ad6481262f216a53c3b3f
George Legeza <glegeza@gmail.com>
2016-03-27 20:39:25 -0400

    Add separate TileDropperController script
    Moved input-related functionality that used to be in TileDropper over to it. Also added a key to switch between dropping physics-controlled and kinematic tiles.

--- Assets/Scripts/TileDropper.cs
+++ Assets/Scripts/TileDropper.cs
@@ -3,52 +3,18 @@ using System.Collections.Generic;
 
 public class TileDropper : MonoBehaviour
 {
-    public KeyCode ResetKey;
-    public List<GameObject> TilePrefabs;
-    public int SelectedTile = 0;
-    public bool RandomTile = true;
-
-    private MouseObjectTracker _tracker;
-    private PixelMouse _mouse;
     private List<GameObject> _tiles = new List<GameObject>();
-    
-	void Start ()
-    {
-        _tracker = FindObjectOfType<MouseObjectTracker>();
-        _mouse = FindObjectOfType<PixelMouse>();
-	}
-	
-	void Update ()
-    {
-        if (Input.GetMouseButtonDown(0) && _tracker.CurrentObject == null)
-        {
-            DropAtLocation();
-        }
-        else if (Input.GetMouseButtonDown(1) && _tracker.CurrentObject != null && _tracker.CurrentObject.GetComponent<HoverColorizer>() != null)
-        {
-            Destroy(_tracker.CurrentObject);
-        }
-        else if (Input.GetKeyDown(ResetKey))
-        {
-            RemoveAllTiles();
-        }
-	}
 
-    private void DropAtLocation()
+    public void DropAtLocation(Vector3 location, GameObject tilePrefab, bool isKinematic = false)
     {
-        var selectedTile = SelectedTile;
-        if (RandomTile)
-        {
-            selectedTile = Random.Range(0, TilePrefabs.Count);
-        }
-        var newTile = Instantiate(TilePrefabs[selectedTile]) as GameObject;
-        var location = _mouse.GetMouseWorldLocation();
+        var newTile = Instantiate(tilePrefab) as GameObject;
+        newTile.GetComponent<Rigidbody2D>().isKinematic = isKinematic;
         location.z = newTile.transform.position.z;
         newTile.transform.position = location;
         _tiles.Add(newTile);
     }
 
-    private void RemoveAllTiles()
+    public void RemoveAllTiles()
     {
         foreach (var tile in _tiles)
         {
---
+++ Assets/Scripts/TileDropperController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(TileDropper))]
+public class TileDropperController : MonoBehaviour
+{
+    public KeyCode ResetKey;
+    public KeyCode KinematicKey;
+    public List<GameObject> TilePrefabs;
+    public int SelectedTile = 0;
+    public bool RandomTile = true;
+    public bool KinematicTiles = false;
+
+    private MouseObjectTracker _tracker;
+    private PixelMouse _mouse;
+    private TileDropper _dropper;
+
+    void Start ()
+    {
+        _tracker = FindObjectOfType<MouseObjectTracker>();
+        _mouse = FindObjectOfType<PixelMouse>();
+        _dropper = GetComponent<TileDropper>();
+	}
+	
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0) && _tracker.CurrentObject == null)
+        {
+            SpawnObject();
+        }
+        else if (Input.GetMouseButtonDown(1) && _tracker.CurrentObject)
+        {
+            KillObject(_tracker.CurrentObject);
+        }
+
+        if (Input.GetKeyDown(ResetKey))
+        {
+            _dropper.RemoveAllTiles();
+        }
+        else if (Input.GetKeyDown(KinematicKey))
+        {
+            KinematicTiles = !KinematicTiles;
+        }
+    }
+
+    void SpawnObject()
+    {
+        var selectedTile = RandomTile ? Random.Range(0, TilePrefabs.Count) : SelectedTile;
+        _dropper.DropAtLocation(_mouse.GetMouseWorldLocation(), TilePrefabs[selectedTile], KinematicTiles);
+    }
+
+    void KillObject(GameObject obj)
+    {
+        if (obj.GetComponent<HoverColorizer>() != null)
+        {
+            Destroy(obj);
+        }
+    }
+}
