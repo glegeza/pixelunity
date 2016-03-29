fdd53edb0098b60255ca033041525713c5c50ede
George Legeza <glegeza@gmail.com>
2016-03-29 14:46:21 -0400

    Set up simple test for square grid tiles

---
+++ Assets/Examples/04_SquareTileGridTest/TileLayer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TileLayer : MonoBehaviour
+{
+    public float XStart = 0.0f;
+    public float YStart = 0.0f;
+    public int GridWidth = 1;
+    public int GridHeight = 1;
+    public float TileWidth = 1.0f;
+    public float TileHeight = 1.0f;
+    public List<GameObject> Tiles = new List<GameObject>();
+    
+	void Start ()
+    {
+        LayTiles();
+	}
+
+    private void LayTiles()
+    {
+        for (var x = XStart; x < XStart + GridWidth; x++)
+        {
+            for (var y = YStart; y < YStart + GridHeight; y++)
+            {
+                var xPos = x * TileWidth;
+                var yPos = y * TileHeight;
+                var nextTile = Instantiate(GetNextTile()) as GameObject;
+                nextTile.transform.position = new Vector3(
+                    xPos, yPos, nextTile.transform.position.z);
+            }
+        }
+    }
+
+    private GameObject GetNextTile()
+    {
+        var idx = Random.Range(0, Tiles.Count);
+        return Tiles[idx];
+    }
+}
