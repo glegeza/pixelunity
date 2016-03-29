1153ab37a9b7ab3a5a17d3494612d2e7b8a7050f
George Legeza <glegeza@gmail.com>
2016-03-29 14:57:51 -0400

    Add simple script to expand a tile on mouse hover

---
+++ Assets/Examples/Shared/Scripts/HoverExpando.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoverExpando : MonoBehaviour
+{
+    public float ExpandoFactor = 2.0f;
+
+    private MouseObjectTracker _tracker;
+
+    void Start()
+    {
+        _tracker = FindObjectOfType<MouseObjectTracker>();
+    }
+
+    void Update()
+    {
+        if (!_tracker)
+        {
+            return;
+        }
+        if (_tracker.CurrentObject == gameObject)
+        {
+            transform.localScale = new Vector3(ExpandoFactor, ExpandoFactor, 1.0f);
+            transform.position = new Vector3(transform.position.x, transform.position.y, -4.0f);
+        }
+        else
+        {
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            transform.position = new Vector3(transform.position.x, transform.position.y, -1.0f);
+        }
+    }
+}
